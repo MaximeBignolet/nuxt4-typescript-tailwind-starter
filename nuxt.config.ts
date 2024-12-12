0f00ee93b22b213f24a13a2f7da94895a968125f
MaximeBignolet <maxime.bignolet@rhinos-groupe.com>
2024-12-12 11:09:14 +0100

    fix(eslint): fix some issues eslint + delete some eslint packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from "nuxt/config";
 
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
   compatibilityDate: `2024-04-03`,
   ssr: false,
   devtools: { enabled: true },
   modules: [ `@nuxtjs/tailwindcss`, `@nuxt/eslint` ],
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  },
   components: [
     {
       path: `~/components`,
